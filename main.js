3108701bd10c1c6e1196c60c4a0280f0bc0d81ac
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-08-08 13:56:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function preload(){
+noseX= 0;
+noseY= 0;
 
+function preload(){
+nose_image= loadImage('https://i.postimg.cc/L8XWWnBW/m.png');
 }
 
 function setup(){
-    canvas= createCanvas(400, 300);
+    canvas= createCanvas(300, 300);
     canvas.center();
     video= createCapture(VIDEO);
     video.size(300, 300);
@@ -11,22 +14,21 @@ function setup(){
     poseNet= ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+ function gotPoses(result){
+if(result.length > 0){
+    console.log(result);
+    noseX= result[0].pose.nose.x - 20 
+    noseY= result[0].pose.nose.y -5
+}
+ }
 
 function modelLoaded(){
     console.log("model Loaded!")
 }
 
-function gotPoses(result){
-    if(result.length > 0){
-        console.log(result);
-        console.log("x value:"+ result[0].pose.nose.x);
-        console.log("y value:"+ result[0].pose.nose.y);
-    }
-}
-
-
 function draw(){
-    image(video, 0, 0, 400, 300);
+image(video, 0, 0, 300, 300);
+image(nose_image, noseX, noseY, 40, 40)
 }
 
 function take_snapshot(){
